35dba5b5b6163f0c8872156e24de05d7a77386fc
Sameer Verma <your-email@example.com>
2026-07-15 17:28:36 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -14,4 +14,7 @@ def chat():
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
